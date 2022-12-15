535eca7b008e36cbe0f6a1649fd144f6b27ae710
Tiago Rodrigues <tpr09756@gmail.com>
2022-12-15 17:28:30 -0100

    first commit

---
+++ Javascript/week2/MathObjectDemo/script.js
@@ -0,0 +1,13 @@
+//generate a number between 0 and 0.99
+Math.random();
+
+//assign it to a variable;
+var decimal = Math.random()
+//log the value to the console;
+console.log(decimal);
+//Log a random number between 0 and 10
+console.log(decimal * 10);
+
+//generate a random integer between 0 and 100
+var integer = Math.ceil(Math.random()*100);
+console.log(integer);
---
+++ Javascript/week2/Practicing with arrays/script.js
@@ -0,0 +1,16 @@
+var clothes = [];
+
+clothes.push('Socks');
+clothes.push('T-shirt');
+clothes.push('Pants');
+clothes.push('Pullover');
+clothes.push('Boxers');
+
+clothes.pop();
+clothes.push('Gloves');
+console.log(clothes);
+
+var favCar={};
+favCar.color = 'blue';
+favCar.convertible = true;
+console.log(favCar);
